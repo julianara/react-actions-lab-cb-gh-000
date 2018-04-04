eb968d2ea58f5c8fc066a4011a7df33d99339ac8
Julia Nara <julianara888@gmail.com>
2018-04-04 02:22:26 +0000

    automatically backed up by learn

--- actions/index.js
+++ actions/index.js
@@ -15,7 +15,7 @@ function addRow (ev) {
   ev.preventDefault();
   
   this.setState({
-    table: [...this.state.table, this.state.table[0].map(() => ' ')]
+    table: [...this.state.table, this.state.table[0].map(() => '')]
   })
 }
 
